c9af1bdbec20ae6e1650a51908ee4407ec2a5c3a
Brian Karcher <bkarcher@colony-games.com>
2018-07-26 18:16:00 -0700

    Tile collider updates.

--- src/RQ.Unity/Assets/Source/Editor/Tilemap Importer.cs
+++ src/RQ.Unity/Assets/Source/Editor/Tilemap Importer.cs
@@ -118,84 +118,107 @@ namespace RQ2.Editor._2D_Toolkit
                 // Get triangles and vertices from mesh
                 var triangles = coll.sharedMesh.triangles;
                 var vertices = coll.sharedMesh.vertices;
-                // Get just the outer edges from the mesh's triangles (ignore or remove any shared edges)
-                var edges = new Dictionary<string, KeyValuePair<int, int>>();
-                for (int i = 0; i < triangles.Length; i += 3)
+                //PolygonCollider2D polygonCollider = _tileMap.renderData.gameObject.AddComponent<PolygonCollider2D>();
+                var go = new GameObject("poly collider");
+                go.transform.SetParent(coll.gameObject.transform);
+
+                PolygonCollider2D polygonCollider = go.AddComponent<PolygonCollider2D>();
+                List<Vector2> myPoints = new List<Vector2>();
+                int pathCount = 0;
+                for (int i = 0; i < triangles.Count(); i++)
                 {
-                    for (int e = 0; e < 3; e++)
+                    myPoints.Add(vertices[triangles[i]]);
+                    if (i % 3 == 0)
                     {
-                        int vert1 = triangles[i + e];
-                        int vert2 = triangles[i + e + 1 > i + 2 ? i : i + e + 1];
-                        string edge = Mathf.Min(vert1, vert2) + ":" + Mathf.Max(vert1, vert2);
-                        if (edges.ContainsKey(edge))
-                        {
-                            edges.Remove(edge);
-                        }
-                        else
-                        {
-                            edges.Add(edge, new KeyValuePair<int, int>(vert1, vert2));
-                        }
-                    }
-                }
-
-                // Create edge lookup (Key is first vertex, Value is second vertex, of each edge)
-                var lookup = new Dictionary<int, int>();
-                foreach (KeyValuePair<int, int> edge in edges.Values)
-                {
-                    if (lookup.ContainsKey(edge.Key) == false)
-                        lookup.Add(edge.Key, edge.Value);
-                }
-                // Create empty polygon collider
-                //GameObject.Add
-                PolygonCollider2D polygonCollider = _tileMap.renderData.gameObject.AddComponent<PolygonCollider2D>();
-                polygonCollider.pathCount = 0;
-                // Loop through edge vertices in order
-                int startVert = 0;
-                int nextVert = startVert;
-                int highestVert = startVert;
-                var colliderPath = new List<Vector2>();
-                while (true)
-                {
-                    // Add vertex to collider path
-                    colliderPath.Add(vertices[nextVert]);
-
-                    // Get next vertex
-                    nextVert = lookup[nextVert];
-
-                    // Store highest vertex (to know what shape to move to next)
-                    if (nextVert > highestVert)
-                    {
-                        highestVert = nextVert;
-                    }
-
-                    // Shape complete
-                    if (nextVert == startVert)
-                    {
-                        // Add path to polygon collider
                         polygonCollider.pathCount++;
-                        polygonCollider.SetPath(polygonCollider.pathCount - 1, colliderPath.ToArray());
-                        colliderPath.Clear();
-
-                        // Go to next shape if one exists
-                        if (lookup.ContainsKey(highestVert + 1))
-                        {
-                            // Set starting and next vertices
-                            startVert = highestVert + 1;
-                            nextVert = startVert;
-
-                            // Continue to next loop
-                            continue;
-                        }
-
-                        // No more verts
-                        break;
+                        polygonCollider.SetPath(polygonCollider.pathCount - 1, myPoints.ToArray());
+                        myPoints.Clear();
                     }
                 }
+
+                //polygonCollider.points = myPoints.ToArray();
+                //polygonCollider.pathCount = 1;
+                //polygonCollider.SetPath(0, myPoints.ToArray());
+
+
+                // Get just the outer edges from the mesh's triangles (ignore or remove any shared edges)
+                //var edges = new Dictionary<string, KeyValuePair<int, int>>();
+                //for (int i = 0; i < triangles.Length; i += 3)
+                //{
+                //    for (int e = 0; e < 3; e++)
+                //    {
+                //        int vert1 = triangles[i + e];
+                //        int vert2 = triangles[i + e + 1 > i + 2 ? i : i + e + 1];
+                //        string edge = Mathf.Min(vert1, vert2) + ":" + Mathf.Max(vert1, vert2);
+                //        if (edges.ContainsKey(edge))
+                //        {
+                //            edges.Remove(edge);
+                //        }
+                //        else
+                //        {
+                //            edges.Add(edge, new KeyValuePair<int, int>(vert1, vert2));
+                //        }
+                //    }
+                //}
+
+                //// Create edge lookup (Key is first vertex, Value is second vertex, of each edge)
+                //var lookup = new Dictionary<int, int>();
+                //foreach (KeyValuePair<int, int> edge in edges.Values)
+                //{
+                //    if (lookup.ContainsKey(edge.Key) == false)
+                //        lookup.Add(edge.Key, edge.Value);
+                //}
+                //// Create empty polygon collider
+                ////GameObject.Add
+                //PolygonCollider2D polygonCollider = _tileMap.renderData.gameObject.AddComponent<PolygonCollider2D>();
+                //polygonCollider.pathCount = 0;
+                //// Loop through edge vertices in order
+                //int startVert = 0;
+                //int nextVert = startVert;
+                //int highestVert = startVert;
+                //var colliderPath = new List<Vector2>();
+                //while (true)
+                //{
+                //    // Add vertex to collider path
+                //    colliderPath.Add(vertices[nextVert]);
+
+                //    // Get next vertex
+                //    nextVert = lookup[nextVert];
+
+                //    // Store highest vertex (to know what shape to move to next)
+                //    if (nextVert > highestVert)
+                //    {
+                //        highestVert = nextVert;
+                //    }
+
+                //    // Shape complete
+                //    if (nextVert == startVert)
+                //    {
+                //        // Add path to polygon collider
+                //        polygonCollider.pathCount++;
+                //        polygonCollider.SetPath(polygonCollider.pathCount - 1, colliderPath.ToArray());
+                //        colliderPath.Clear();
+
+                //        // Go to next shape if one exists
+                //        if (lookup.ContainsKey(highestVert + 1))
+                //        {
+                //            // Set starting and next vertices
+                //            startVert = highestVert + 1;
+                //            nextVert = startVert;
+
+                //            // Continue to next loop
+                //            continue;
+                //        }
+
+                //        // No more verts
+                //        break;
+                //    }
+                //}
 
                 // myPoints.Clear();
                 //Vector2[] myPoints = coll.points;
                 //int myEdges = coll.edgeCount;
-                
+
                 //myPoly.points = myPoints;
                 //myPoly.pathCount = 1;
                 //myPoly.SetPath(0, myPoints);
