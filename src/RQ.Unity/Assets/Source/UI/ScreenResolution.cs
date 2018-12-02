0cd1abd14c1c0b88cbf3866bdd5d24e78650f054
Brian Karcher <bkarcher@colony-games.com>
2018-12-02 00:40:18 -0800

    AppStart updates.

---
+++ src/RQ.Unity/Assets/Source/UI/ScreenResolution.cs
@@ -0,0 +1,57 @@
+﻿using RQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Source.UI
+{
+    [AddComponentMenu("RQ/UI/Screen Resolution")]
+    public class ScreenResolution : MonoBehaviour
+    {
+        public UILabel Resolution;
+        public UIPopupList ResolutionPopup;
+        public UIToggle FullScreenCheckbox;
+
+        public void Start()
+        {
+            ResolutionPopup.itemData = new List<object>() { "0", "1" };
+            int width = 1152;
+            int height = 648;
+            GameController.Instance.GetGraphicsEngine().SetScreenResolution(width, height, false);
+        }
+
+        public void ProcessNewResolution()
+        {
+            int width;
+            int height;
+            bool fullScreen;
+            if (FullScreenCheckbox.value)
+            {
+                var highestRes = GameController.Instance.GetGraphicsEngine().GetHighestResolution();
+                width = highestRes.width;
+                height = highestRes.height;
+                fullScreen = true;
+            }
+            else
+            {
+                
+                //576x432
+                //864x648
+                if ((string)ResolutionPopup.data == "0")
+                {
+                    width = 576;
+                    height = 432;
+                }
+                else
+                {
+                    width = 1152;
+                    height = 648;
+                }
+                fullScreen = false;
+            }
+            GameController.Instance.GetGraphicsEngine().SetScreenResolution(width, height, fullScreen);
+        }
+    }
+}
