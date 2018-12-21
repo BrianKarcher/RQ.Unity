ab422694e899925c8b6c63e8f0024cd7f1a41611
Brian Karcher <bkarcher@colony-games.com>
2018-12-20 16:23:53 -0800

    Upgraded Rewired. Wired up action Action. FIxed size issue of the Control Mapper.

---
+++ src/RQ.Unity/Assets/Source/UI/CanvasScalerEnforcer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RQ2.Controller.UI
+{
+    [AddComponentMenu("RQ/UI/Canvas Scaler Enforcer")]
+    public class CanvasScalerEnforcer : MonoBehaviour
+    {
+        public CanvasScaler canvasScaler;
+        //[SerializeField]
+        //private UIScrollView _scrollView;
+        //[SerializeField]
+        //private float _strength = 1f;
+
+        public void OnUpdate()
+        {
+            canvasScaler.referenceResolution = new Vector2(800, 600);
+        }
+
+        //public void OnHover(bool selected)
+        //{
+        //    if (!selected)
+        //        return;
+
+        //    if (_scrollView == null)
+        //        return;
+
+        //    if (UICamera.currentScheme == UICamera.ControlScheme.Controller)
+        //    {
+        //        // Make sure this object is visible in the scroll view
+        //        // when selected
+        //        SpringPanel.Begin(_scrollView.gameObject,
+        //            new Vector3(transform.localPosition.x, -transform.localPosition.y, transform.localPosition.z),
+        //            _strength);
+        //        //_scrollView.
+        //    }
+        //}
+
+        //public void SetScrollView(UIScrollView scrollView)
+        //{
+        //    _scrollView = scrollView;
+        //}
+    }
+}
