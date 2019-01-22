ca305165275d1f07d1aef1a5864895e4939a339e
Brian Karcher <bkarcher@colony-games.com>
2019-01-22 15:39:23 -0800

    TSX test.

---
+++ src/RQ.Unity/Assets/Areas/South/Bog Maze/Maps/Bog-maze_tiles.tsx
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Bog-maze_tiles" tilewidth="16" tileheight="16" tilecount="1855" columns="53">
+ <image source="Bog maze tileset.png" width="848" height="560"/>
+ <terraintypes>
+  <terrain name="Sand" tile="73"/>
+  <terrain name="Water1" tile="81"/>
+  <terrain name="Grass1" tile="217"/>
+ </terraintypes>
+ <tile id="19" terrain=",,,0"/>
+ <tile id="20" terrain=",,0,0"/>
+ <tile id="21" terrain=",,0,"/>
+ <tile id="22" terrain=",0,0,0"/>
+ <tile id="23" terrain="0,,0,0"/>
+ <tile id="27" terrain=",,,1"/>
+ <tile id="28" terrain=",,1,1"/>
+ <tile id="29" terrain=",,1,"/>
+ <tile id="30" terrain="1,1,1,"/>
+ <tile id="31" terrain="1,1,,1"/>
+ <tile id="72" terrain=",0,,0"/>
+ <tile id="73" terrain="0,0,0,0"/>
+ <tile id="74" terrain="0,,0,"/>
+ <tile id="75" terrain="0,0,,0"/>
+ <tile id="76" terrain="0,0,0,"/>
+ <tile id="80" terrain=",1,,1"/>
+ <tile id="81" terrain="1,1,1,1"/>
+ <tile id="82" terrain="1,,1,"/>
+ <tile id="83" terrain="1,,1,1"/>
+ <tile id="84" terrain=",1,1,1"/>
+ <tile id="125" terrain=",0,,"/>
+ <tile id="126" terrain="0,0,,"/>
+ <tile id="127" terrain="0,,,"/>
+ <tile id="133" terrain=",1,,"/>
+ <tile id="134" terrain="1,1,,"/>
+ <tile id="135" terrain="1,,,"/>
+ <tile id="160" terrain=",2,2,2"/>
+ <tile id="161" terrain="2,,2,2"/>
+ <tile id="164" terrain=",,2,2"/>
+ <tile id="166" terrain=",,,2"/>
+ <tile id="169" terrain=",,2,"/>
+ <tile id="216" terrain=",2,,2"/>
+ <tile id="217" terrain="2,2,2,2"/>
+ <tile id="218" terrain="2,,2,"/>
+ <tile id="219" terrain=",2,,"/>
+ <tile id="222" terrain="2,,,"/>
+ <tile id="270" terrain="2,2,,"/>
+ <tile id="319" terrain="2,2,,2"/>
+ <tile id="320" terrain="2,2,2,"/>
+</tileset>
