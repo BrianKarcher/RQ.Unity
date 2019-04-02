58a38a562cd2b3b18f0dd2f3a12cc213854e2b02
MiguelExposito <miguelexposito3d@gmail.com>
2019-04-02 17:12:06 +0200

    Cave Tileset

---
+++ src/RQ.Unity/Assets/Areas/South/Bog Maze/Maps/BogMaze Tileset - Underground.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="BogMaze Tileset - Underground" tilewidth="16" tileheight="16" tilecount="1296" columns="36">
+ <image source="Bog Maze Tileset 2 - Underground.png" trans="ff00ff" width="576" height="576"/>
+ <terraintypes>
+  <terrain name="Floor 1" tile="0"/>
+  <terrain name="Top of the Wall 1" tile="15"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="3" terrain="0,0,0,"/>
+ <tile id="4" terrain="0,0,,0"/>
+ <tile id="15" terrain=",,,1"/>
+ <tile id="16" terrain=",,1,1"/>
+ <tile id="17" terrain=",,1,"/>
+ <tile id="18" terrain="1,1,1,"/>
+ <tile id="19" terrain="1,1,,1"/>
+ <tile id="36" terrain=",0,,0"/>
+ <tile id="37" terrain="0,0,0,0"/>
+ <tile id="38" terrain="0,,0,"/>
+ <tile id="39" terrain="0,,0,0"/>
+ <tile id="40" terrain=",0,0,0"/>
+ <tile id="51" terrain=",1,,1"/>
+ <tile id="52" terrain="1,1,1,1"/>
+ <tile id="53" terrain="1,,1,"/>
+ <tile id="72" terrain=",0,,"/>
+ <tile id="73" terrain="0,0,,"/>
+ <tile id="74" terrain="0,,,"/>
+ <tile id="87" terrain=",1,,"/>
+ <tile id="88" terrain="1,1,,"/>
+ <tile id="89" terrain="1,,,"/>
+ <tile id="198" terrain="1,,1,1"/>
+ <tile id="199" terrain=",1,1,1"/>
+</tileset>
