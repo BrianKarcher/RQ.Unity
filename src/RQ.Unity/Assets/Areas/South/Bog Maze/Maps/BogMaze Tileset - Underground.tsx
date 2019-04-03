e6a8506e31a0b025620765122cfbae715b31e3ea
MiguelExposito <miguelexposito3d@gmail.com>
2019-04-03 12:31:52 +0200

    Update Underground sections.

--- src/RQ.Unity/Assets/Areas/South/Bog Maze/Maps/BogMaze Tileset - Underground.tsx
+++ src/RQ.Unity/Assets/Areas/South/Bog Maze/Maps/BogMaze Tileset - Underground.tsx
@@ -3,32 +3,60 @@
  <image source="Bog Maze Tileset 2 - Underground.png" trans="ff00ff" width="576" height="576"/>
  <terraintypes>
   <terrain name="Floor 1" tile="0"/>
+  <terrain name="Thick Floor 1" tile="216"/>
   <terrain name="Top of the Wall 1" tile="15"/>
+  <terrain name="Wall Floor 1" tile="447"/>
  </terraintypes>
  <tile id="0" terrain=",,,0"/>
  <tile id="1" terrain=",,0,0"/>
  <tile id="2" terrain=",,0,"/>
  <tile id="3" terrain="0,0,0,"/>
  <tile id="4" terrain="0,0,,0"/>
- <tile id="15" terrain=",,,1"/>
- <tile id="16" terrain=",,1,1"/>
- <tile id="17" terrain=",,1,"/>
- <tile id="18" terrain="1,1,1,"/>
- <tile id="19" terrain="1,1,,1"/>
+ <tile id="15" terrain=",,,2"/>
+ <tile id="16" terrain=",,2,2"/>
+ <tile id="17" terrain=",,2,"/>
+ <tile id="18" terrain="2,2,2,"/>
+ <tile id="19" terrain="2,2,,2"/>
  <tile id="36" terrain=",0,,0"/>
  <tile id="37" terrain="0,0,0,0"/>
  <tile id="38" terrain="0,,0,"/>
  <tile id="39" terrain="0,,0,0"/>
  <tile id="40" terrain=",0,0,0"/>
- <tile id="51" terrain=",1,,1"/>
- <tile id="52" terrain="1,1,1,1"/>
- <tile id="53" terrain="1,,1,"/>
+ <tile id="51" terrain=",2,,2"/>
+ <tile id="52" terrain="2,2,2,2"/>
+ <tile id="53" terrain="2,,2,"/>
  <tile id="72" terrain=",0,,"/>
  <tile id="73" terrain="0,0,,"/>
  <tile id="74" terrain="0,,,"/>
- <tile id="87" terrain=",1,,"/>
- <tile id="88" terrain="1,1,,"/>
- <tile id="89" terrain="1,,,"/>
- <tile id="198" terrain="1,,1,1"/>
- <tile id="199" terrain=",1,1,1"/>
+ <tile id="87" terrain=",2,,"/>
+ <tile id="88" terrain="2,2,,"/>
+ <tile id="89" terrain="2,,,"/>
+ <tile id="198" terrain="2,,2,2"/>
+ <tile id="199" terrain=",2,2,2"/>
+ <tile id="216" terrain=",,,1"/>
+ <tile id="217" terrain=",,1,1"/>
+ <tile id="218" terrain=",,1,"/>
+ <tile id="219" terrain="1,1,1,"/>
+ <tile id="220" terrain="1,1,,1"/>
+ <tile id="252" terrain=",1,,1"/>
+ <tile id="253" terrain="1,1,1,1"/>
+ <tile id="254" terrain="1,,1,"/>
+ <tile id="255" terrain="1,,1,1"/>
+ <tile id="256" terrain=",1,1,1"/>
+ <tile id="288" terrain=",1,,"/>
+ <tile id="289" terrain="1,1,,"/>
+ <tile id="290" terrain="1,,,"/>
+ <tile id="447" terrain=",,,3"/>
+ <tile id="448" terrain=",,3,3"/>
+ <tile id="449" terrain=",,3,"/>
+ <tile id="450" terrain="3,3,3,"/>
+ <tile id="451" terrain="3,3,,3"/>
+ <tile id="483" terrain=",3,,3"/>
+ <tile id="484" terrain="3,3,3,3"/>
+ <tile id="485" terrain="3,,3,"/>
+ <tile id="486" terrain="3,,3,3"/>
+ <tile id="487" terrain=",3,3,3"/>
+ <tile id="519" terrain=",3,,"/>
+ <tile id="520" terrain="3,3,,"/>
+ <tile id="521" terrain="3,,,"/>
 </tileset>
